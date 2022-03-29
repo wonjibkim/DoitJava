86227b650db3a972af7a800f612cd95717ebfd7b
wonjibkim <kbg5174@naver.com>
2022-03-29 22:03:51 +0900

    3.29 학습내용

---
+++ src/main/java/test002.java
@@ -0,0 +1,10 @@
+public class test002 {
+    public static void main(String[] args) {
+
+        int x = 50;
+        int y = 60;
+
+        if (x < y)
+            System.out.println(" x는 y보다 작습니다");
+    }
+}
---
+++ src/main/java/test003.java
@@ -0,0 +1,11 @@
+public class test003 {
+    public static void main(String[] args) {
+
+        int a = 150;
+        int b = 150;
+
+        if (a == b) {
+            System.out.println("x는 y랑 같습니다");
+        }
+    }
+}
