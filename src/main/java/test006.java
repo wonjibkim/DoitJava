f26f7be6bab6d14612c0dcc0548ec17d661203fd
wonjibkim <kbg5174@naver.com>
2022-04-01 15:03:48 +0900

    깃허브

--- src/main/java/test006.java
+++ src/main/java/test006.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
 public class test006 {
+
     public static void main(String args[]) {
         Scanner sc = new Scanner(System.in);
         for (int i =1; i < 10; i++){ // 1 2 3
@@ -18,3 +19,24 @@ public class test006 {
         // 1 ~ 9 1 ~ 9
     }
 }
+/*
+//=======
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = scanner.nextInt();
+            if (arr[i] % 2 == 0) {
+                System.out.println("even");
+            } else {
+                System.out.println("odd");
+
+            }
+        }
+    }
+}
+>>>>>>> 257cf0e (멘토링)
+*/
