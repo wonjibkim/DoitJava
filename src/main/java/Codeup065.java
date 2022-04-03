0ae5d36c957d33ccae2b4ab4f3e2b96fe38163df
wonjibkim <kbg5174@naver.com>
2022-04-04 00:24:24 +0900

    4.4 학습내용

---
+++ src/main/java/Codeup065.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Codeup065 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a  = sc.nextInt();
+        int b  = sc.nextInt();
+        int c  = sc.nextInt();
+
+        if (a % 2 == 0)
+            System.out.println(a);
+
+        if (b % 2 == 0)
+            System.out.println(b);
+
+        if (c % 2 == 0)
+            System.out.println(c);
+
+
+    }
+}
+//   Scanner s = new Scanner(System.in);
+//
+//        System.out.println("점수를 입력하시오");
+//        int a = s.nextInt();
