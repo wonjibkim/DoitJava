bb35ecc22edaf90cf1b1d81c286c5edbc5ee252d
wonjibkim <kbg5174@naver.com>
2022-04-07 21:48:47 +0900

    4/7 학습내용

---
+++ src/main/java/Test009.java
@@ -0,0 +1,30 @@
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
+public class Test009 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("점수를 입력하시오");
+        int a = s.nextInt();
+
+        if (a >= 90) {
+            System.out.println("A");}
+
+        else
+            if (a >= 80) {
+                System.out.println("B");
+            }
+            else
+                if ( a >= 70)
+                {
+                    System.out.println("C");}
+                    else
+                        if ( a >= 60)
+                        { System.out.println("D");}
+
+
+
+    }
+}
+// 복습
