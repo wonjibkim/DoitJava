7b2cbf6b6667958f66261925e373b02e9c5ec8d0
wonjibkim <kbg5174@naver.com>
2022-05-09 19:37:13 +0900

    5.09 - 자바

---
+++ src/main/java/AA.java
@@ -0,0 +1,19 @@
+import com.sun.deploy.security.SelectableSecurityManager;
+
+public class AA {
+
+    public static void main(String[] args) {
+
+        int sum = 0;
+
+        for (int i=50; i<101; i++){
+            if(i % 3 ==0 || i % 7==0 || i % 11==0);
+
+
+                else
+                    sum += i;
+
+        }
+        System.out.printf("합  : %d", sum);
+    }
+}
---
+++ src/main/java/BB.java
@@ -0,0 +1,14 @@
+public class BB {
+    public static void main(String[] args) {
+
+        for (int i =1; i<6; i++)// 1부터 6까지 증가하는 for문을 사용하여 요구조건 5행을 충족 시킴
+        {
+            for (int j =1; j<=i ; j++){
+                System.out.printf("*");            }
+            System.out.println("");
+        }
+
+
+
+    }
+}
