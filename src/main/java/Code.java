5f736fe307a488a003eebdf4bb52264849d87e84
wonjibkim <kbg5174@naver.com>
2022-06-02 22:32:45 +0900

    6/2 학습내용

---
+++ src/main/java/Code.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+
+public class Code {
+    {
+
+         Scanner sc = new Scanner(System.in);
+
+        while(true)
+        {
+            int a = sc.nextInt();
+            if (a!=0)
+            {
+                System.out.println(a);
+            }
+            else
+            {
+                break;
+            }
+        }
+    }
+}
+
