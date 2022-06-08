811fe7398a918b427c35afa41769d2d349356b3d
wonjibkim <kbg5174@naver.com>
2022-06-08 13:05:32 +0900

    0608 자바수업

---
+++ src/main/java/Codeup/test01.java
@@ -0,0 +1,14 @@
+package Codeup;
+
+public class test01 {
+    public static void main(String[] args) {
+
+        String log = "111.222.33.44 - - [11/Jun/2012:22:12:32 +0900] \"GET /w/custom/jmnote20120124.ico HTTP/1.1\" 304 - \"-\" \"Mozilla/4.0 (compatible; MSIE 7.0; Windows NT 6.1; Trident/5.0; SLCC2; .NET CLR 2.0.50727; .NET CLR 3.5.30729; .NET CLR 3.0.30729; .NET4.0C; MS-RTC LM 8)\"";
+
+                String [] item = log.split(" ");
+        System.out.println(item[0]);
+
+        System.out.println(item[3].substring(1));
+
+    }
+}
