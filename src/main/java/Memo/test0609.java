1c576e191a4662c3f5c0d0dd409df50eccbf4f82
wonjibkim <kbg5174@naver.com>
2022-06-17 14:01:54 +0900

    6/16 프로그래머스

--- src/main/java/Memo/test0609.java
+++ src/main/java/Memo/test0609.java
@@ -11,24 +11,33 @@ public class test0609 {
 
 
         if(str.startsWith("zero"))
-        str2 = str.replace("zero" , "1");
-        else if(str.startsWith("one"))
+        str2 = str.replace("zero" , "0");
+
+        if(str.startsWith("one"))
         str2 = str.replace("one","1");
-        else if(str.startsWith("two"))
+
+        if(str.startsWith("two"))
             str2 = str.replace("two", "2");
-        else if(str.startsWith("three"))
+
+        if(str.startsWith("three"))
             str2 = str.replace("three","3");
-        else if(str.startsWith("four"))
-            str2 = str.replace("foru","four");
-        else if(str.startsWith("five"))
+
+         if(str.startsWith("four"))
+            str2 = str.replace("foru","4");
+
+        if(str.startsWith("five"))
             str2 = str.replace("five","5");
-        else if(str.startsWith("six"))
+
+         if(str.startsWith("six"))
             str2 =  str.replace("six","6");
-        else if(str.startsWith("seven"))
+
+         if(str.startsWith("seven"))
             str2 = str.replace("seven","7");
-        else if(str.startsWith("eight"))
+
+        if(str.startsWith("eight"))
             str2 = str.replace("eight","8");
-        else if(str.startsWith("nine"))
+
+         if(str.startsWith("nine"))
             str2 = str.replace("nine","9");
 
         System.out.println(str2);
